4788d1d475e4ea4f2f0f166fef6a2d1f159c2c93
swiminspace <andre.michell@usma.edu>
2017-07-06 18:16:34 -0400

    Preparation for deployment to PyPI
    Includes updates to README and improvements to __init__.py to enable
    streamlined installation and enabling once downloaded via PyPI

--- ordo/__init__.py
+++ ordo/__init__.py
@@ -1,6 +1,6 @@
 def _jupyter_nbextension_paths():
-  return [dict(
-      'section'="notebook",
-      'src'="static",
-      'dest'="ordo",
-      'require'='ordo/ordo.js')]
+  return [{
+      'section': "notebook",
+      'src': "",
+      'dest': "ordo",
+      'require': 'ordo/ordo'}]
