8c8fced6ab0319aebf8e068fdf6d59847383ac8f
swiminspace <andre.j.michell2@gmail.com>
2017-07-07 20:51:36 +0000

    bump version, metadata adjustments

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup (
 	name = 'ordo',
 	packages = ['ordo'],
-	version = '0.1.5',
-	description = 'A lightweight grading tool for nbgallery',
+	version = '0.1.7',
+	description = 'A lightweight feedback tool for Jupyter',
 	author = 'Andre J. Michell',
 	author_email = 'andre.j.michell2@gmail.com',
 	url = 'https://github.com/nbgallery/ordo',
