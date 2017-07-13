68be0ee321869d4e697ca9e9dc4c7b7c17a64c7e
swiminspace <andre.j.michell2@gmail.com>
2017-07-13 14:40:14 +0000

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup (
 	name = 'ordo',
 	packages = ['ordo'],
-	version = '0.2.0',
+	version = '0.2.1',
 	description = 'A lightweight feedback tool for Jupyter',
 	author = 'Andre J. Michell',
 	author_email = 'andre.j.michell2@gmail.com',
